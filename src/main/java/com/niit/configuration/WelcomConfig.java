94ff8909cd08956eacebdeb80c30be6ae6c070e4
deepthi192 <deepti.manthri@gmail.com>
2017-01-15 23:59:39 +0530

    Add files via upload

---
+++ src/main/java/com/niit/configuration/WelcomConfig.java
@@ -0,0 +1,25 @@
+package com.niit.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.niit")
+public class WelcomConfig {
+    @Bean(name="HelloWorld")
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
